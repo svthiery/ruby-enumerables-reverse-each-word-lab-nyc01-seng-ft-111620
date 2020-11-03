ddd25a079ccecf52cb87a1e17fd3b4c1eaf7bad0
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-03 22:41:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   array = string.split(" ")
   array.collect do |element|
-    new_array << element.reverse
+    element.reverse
   end
-  new_array.join(" ")
+  array.join(" ")
 end
